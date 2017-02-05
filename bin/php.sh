3444da0963554fba045ed391370c52bfe87bde7f
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-05 12:28:14 +0100

    PHP: always start new container

--- bin/php.sh
+++ bin/php.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_CONTAINER=dockerstack_php-fpm_1
-
-if [ -f "$1" ] && [ ! -z "$1" ]; then
-	DOCKER_PATH=$(pwd | sed s~/home/martin/Work~/var/www~)
-	docker exec -it $DOCKER_CONTAINER php "$DOCKER_PATH/$@"
-else
-	docker exec -it $DOCKER_CONTAINER php "$@"
-fi
+docker run --interactive --rm \
+		--volume $PWD:$PWD \
+		--workdir $PWD \
+		x/php-fpm php "$@"
